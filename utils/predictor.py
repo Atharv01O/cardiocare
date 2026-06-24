e63b63cf7ea65ad57be4335555f867dfe9993769
Atharv <atharvadevrukhkar13@gmail.com>
2026-06-25 01:37:09 +0530

    Updated the file in utils

--- utils/predictor.py
+++ utils/predictor.py
@@ -8,8 +8,8 @@ import joblib
 from config import MODEL_PATH, SCALER_PATH
 
 # Load once at import time
-_model = tf.saved_model.load(MODEL_PATH)
-_scaler = joblib.load(SCALER_PATH)
+_heart_model  = tf.keras.models.load_model(MODEL_PATH)
+_heart_scaler = joblib.load(SCALER_PATH)
 
 
 def run_prediction(form_values: list) -> float:
@@ -17,11 +17,7 @@ def run_prediction(form_values: list) -> float:
     Takes a list of 13 raw floats (same order as CSV columns),
     returns a probability float between 0 and 1.
     """
-    arr = np.array(form_values, dtype=float).reshape(1, -1)
-    arr = _scaler.transform(arr)
-    prob = float(_model.predict(arr, verbose=0)[0][0])
-
-# dataset: 1 = healthy, 0 = disease
-    risk = 1 - prob
-
-    return risk
+    arr  = np.array(form_values, dtype=float).reshape(1, -1)
+    arr  = _heart_scaler.transform(arr)
+    prob = float(_heart_model.predict(arr, verbose=0)[0][0])
+    return prob
