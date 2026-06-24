ff70b31cf5ea42014d59353122062d9b9a032e98
Atharv <atharvadevrukhkar13@gmail.com>
2026-06-25 02:52:14 +0530

    Fix saved model prediction function

--- utils/predictor.py
+++ utils/predictor.py
@@ -5,15 +5,20 @@ Loads the trained model + scaler and exposes run_prediction().
 import numpy as np
 import tensorflow as tf
 import joblib
-from config import MODEL_PATH, SCALER_PATH
+from config import SCALER_PATH
 
 _heart_model = tf.saved_model.load("model/saved_model")
 _heart_scaler = joblib.load(SCALER_PATH)
 
 
 def run_prediction(form_values):
-    arr  = np.array(form_values, dtype=float).reshape(1, -1)
-    arr  = _heart_scaler.transform(arr)
+    arr = np.array(form_values, dtype=float).reshape(1, -1)
+    arr = _heart_scaler.transform(arr)
+
     infer = _heart_model.signatures["serving_default"]
-output = infer(tf.constant(arr, dtype=tf.float32))
-prob = float(list(output.values())[0].numpy()[0][0])
+
+    output = infer(tf.constant(arr, dtype=tf.float32))
+
+    prob = float(list(output.values())[0].numpy()[0][0])
+
+    return prob
