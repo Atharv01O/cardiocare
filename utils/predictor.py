84cfea070c1e72d40d0b906affde2b9304c23d4f
Atharv <atharvadevrukhkar13@gmail.com>
2026-06-25 02:02:57 +0530

    Fix predictor to use keras load_model with h5

--- utils/predictor.py
+++ utils/predictor.py
@@ -1,17 +1,18 @@
+"""
+Loads the trained model + scaler and exposes run_prediction().
+"""
+
 import numpy as np
 import tensorflow as tf
-import keras
 import joblib
 from config import MODEL_PATH, SCALER_PATH
 
-_heart_model  = keras.layers.TFSMLayer(MODEL_PATH, call_endpoint='serving_default')
+_heart_model  = tf.keras.models.load_model(MODEL_PATH)
 _heart_scaler = joblib.load(SCALER_PATH)
 
 
-def run_prediction(form_values: list) -> float:
-    arr    = np.array(form_values, dtype=float).reshape(1, -1)
-    arr    = _heart_scaler.transform(arr)
-    output = _heart_model(arr)
-    # TFSMLayer returns a dict — get the first output value
-    prob   = float(list(output.values())[0].numpy()[0][0])
+def run_prediction(form_values):
+    arr  = np.array(form_values, dtype=float).reshape(1, -1)
+    arr  = _heart_scaler.transform(arr)
+    prob = float(_heart_model.predict(arr, verbose=0)[0][0])
     return prob
